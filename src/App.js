91c46d0c4c269ef9c7b6d862c47257913a55f61b
Amol shelke <shelkeamol897@gmail.com>
2023-03-18 20:43:24 +0530

    Tailwind configuration done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-4xl font-serif">Hello</h1>
+    </div>
+  );
 };
 
 export default App;
