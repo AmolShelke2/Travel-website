ffd0cd9bbf2e1ed3990b0848c081591855695ff1
Amol shelke <shelkeamol897@gmail.com>
2023-03-18 23:19:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl font-serif">Hello</h1>
+      <Navbar />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between items-center h-20">
+      <div>
+        <h1>BEACHES</h1>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>Views</li>
+        <li>Book</li>
+      </ul>
+      <div></div>
+    </div>
+  );
 };
 
 export default Navbar;
