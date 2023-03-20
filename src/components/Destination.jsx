3f50d99a00b5d870a366c9f414873c5e36e04f53
Amol shelke <shelkeamol897@gmail.com>
2023-03-20 22:34:22 +0530

    Added destination component

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import Borabora from "../assets/borabora.jpg";
+import Borabora2 from "../assets/borabora2.jpg";
+import Maldives from "../assets/maldives.jpg";
+import Maldives2 from "../assets/maldives2.jpg";
+import Keywest from "../assets/keywest.jpg";
+
 const Destination = () => {
-  return <div>Destination</div>;
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 text-center">
+      <h1>All-Inclusive Resorts</h1>
+      <p>On the Caribbean's Best Beaches</p>
+      <div className="grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4">
+        <img
+          src={Borabora}
+          alt="/"
+          className="w-full h-full object-cover col-span-2 md:col-span-3 row-span-2"
+        />
+        <img src={Borabora2} alt="/" className="w-full h-full object-cover" />
+        <img src={Maldives} alt="/" className="w-full h-full object-cover" />
+        <img src={Maldives2} alt="/" className="w-full h-full object-cover" />
+        <img src={Keywest} alt="/" className="w-full h-full object-cover" />
+      </div>
+    </div>
+  );
 };
 
 export default Destination;
