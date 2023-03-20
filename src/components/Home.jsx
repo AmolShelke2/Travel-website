000e124406ce94f06bb719f4f41c066830c3b209
Amol shelke <shelkeamol897@gmail.com>
2023-03-20 19:31:31 +0530

    Created home section with background video

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import beachVid from "../assets/beachVid.mp4";
 
+import { AiOutlineSearch } from "react-icons/ai";
+
 const Home = () => {
   return (
     <div className="w-full h-screen relative">
@@ -11,6 +13,36 @@ const Home = () => {
         muted
         src={beachVid}
       />
+      <div
+        className="absolute top-0 left-0 w-full h-full
+       bg-gray-700/30"></div>
+      <div
+        className="absolute top-0 left-0 w-full h-full flex 
+      flex-col justify-center item-center text-center text-white p-4">
+        <h1>First class travel</h1>
+        <h2 className="py-3">Top 1% location worldwide</h2>
+        <form
+          className="flex items-center justify-between 
+        max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90">
+          <div>
+            <input
+              className="bg-transparent w-[300px] sm:w-[400px] 
+              font-[Poppins] focus:outline-none"
+              type="text"
+              placeholder="Search Destination"
+            />
+          </div>
+          <div>
+            <button>
+              <AiOutlineSearch
+                size={20}
+                className="icon"
+                style={{ color: "#ffff" }}
+              />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
