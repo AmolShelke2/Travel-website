848689ce2c75dd390b36266793570b84634fe6a3
Amol shelke <shelkeamol897@gmail.com>
2023-03-23 20:12:56 +0530

    imported icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="w-full bg-gray-100 py-16">
       <div className="max-w-[1240px] mx-auto flex flex-col px-4">
         <h1>BEACHES</h1>
+        <div className="">
+          <FaFacebook className="icon" />
+          <FaTwitter className="icon" />
+          <FaLinkedin className="icon" />
+          <FaGithub className="icon" />
+        </div>
       </div>
     </div>
   );
