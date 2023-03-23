961aca72f391abb37c2979962e7f05246954bf85
Amol shelke <shelkeamol897@gmail.com>
2023-03-23 20:06:48 +0530

    Adding tailwind for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full bg-gray-100 py-16">
+      <div className="max-w-[1240px] mx-auto flex flex-col px-4">
+        <h1>BEACHES</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
