1d0a9ea4778fd8bf4af0b348a1e01332d88a0def
Amol shelke <shelkeamol897@gmail.com>
2023-05-07 22:35:51 +0530

    [update]: Text changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="w-full bg-gray-100 py-16">
       <div className="max-w-[1240px] mx-auto flex flex-col px-4">
         <div className="sm:flex text-center justify-between">
-          <h1 className="mb-8">BEACHES</h1>
+          <h1 className="mb-8 uppercase">beaches</h1>
           <div className="flex justify-between w-full sm:max-w-[200px] my-4">
             <FaFacebook className="icon" />
             <FaTwitter className="icon" />
